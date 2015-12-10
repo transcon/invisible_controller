bddb554c1c12942959a4374a50701fbb5144c42f
Chris Moody <cmoody@transcon.com>
2015-12-09 19:13:31 -0800

    include nested attributes in whitelisted

--- lib/invisible_controller/active_record_extension.rb
+++ lib/invisible_controller/active_record_extension.rb
@@ -2,9 +2,17 @@ module ActiveRecordExtension
   extend ActiveSupport::Concern
   module ClassMethods
     def whitelisted()
-      return self::WHITELISTED                if self.constants.include?(:WHITELISTED)
-      return column_names - self::BLACKLISTED if self.constants.include?(:BLACKLISTED)
-      column_names
+      return self::WHITELISTED                                       if self.constants.include?(:WHITELISTED)
+      return column_names_with_nested_attributes - self::BLACKLISTED if self.constants.include?(:BLACKLISTED)
+      column_names_with_nested_attributes
+    end
+    private
+    def column_names_with_nested_attributes
+      deliverable = column_names.dup
+      nested_attributes_options.keys.each do |key|
+        deliverable << {"#{key}_attributes" => key.classify.constantize.whitelisted}
+      end
+      deliverable
     end
   end
 end
